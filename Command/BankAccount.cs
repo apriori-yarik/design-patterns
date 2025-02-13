ca7b4ab0cecdc0927ce4729fcc5b93661602ea27
Yaroslav Dokuzov <yaroslav.dokuzov@primeholding.com>
2025-02-13 23:04:40 +0200

    Command - Example 1

---
+++ Command/BankAccount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command
+{
+    public class BankAccount
+    {
+        private int balance;
+        private int overdraftLimit = -500;
+
+        public BankAccount(int balance = 0)
+        {
+            this.balance = balance;
+        }
+
+        public void Deposit(int amount)
+        {
+            this.balance += amount;
+            Console.WriteLine($"Deposited amount ${amount}, balance is now ${balance}");
+        }
+
+        public bool Withdraw(int amount)
+        {
+            if (balance - amount >= overdraftLimit)
+            {
+                this.balance -= amount;
+                Console.WriteLine($"Withdrew ${amount}, balance is now ${balance}");
+                return true;
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(balance)}: {balance}";
+        }
+    }
+}
---
+++ Command/CompositeBankAccountCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command
+{
+    public abstract class CompositeBankAccountCommand : List<BankAccountCommand>, ICommand
+    {
+        public bool Success { get; set; }
+
+        public virtual void Call()
+        {
+            ForEach(cmd => cmd.Call());
+        }
+
+        public virtual void Undo()
+        {
+            foreach (var cmd in
+              ((IEnumerable<BankAccountCommand>)this).Reverse())
+            {
+                cmd.Undo();
+            }
+        }
+    }
+}
